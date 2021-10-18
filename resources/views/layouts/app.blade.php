cdd6d570dcda3b9020c22f527380e17012e3cc70
jasmin-shukla <mr.jasmin.shukal@gmail.com>
2021-10-18 14:26:20 +0530

    fev icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,11 +12,11 @@
     <link rel="preconnect" href="fonts.gstatic.com/index.html">
     <link href="fonts.googleapis.com/css24d2f.css?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
     <!-- Favicon -->
-    <link rel="icon" href="img/core-img/favicon.ico">
-    <link rel="apple-touch-icon" href="img/icons/icon-96x96.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="img/icons/icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="167x167" href="img/icons/icon-167x167.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="img/icons/icon-180x180.png">
+    <link rel="icon" href="{{asset('tubmasterlogo.ico')}}">
+    <link rel="apple-touch-icon" href="{{asset('tubmasterlogo.ico')}}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{asset('tubmasterlogo.ico')}}">
+    <link rel="apple-touch-icon" sizes="167x167" href="{{asset('tubmasterlogo.ico')}}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('tubmasterlogo.ico')}}">
     <!-- CSS Libraries -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/bootstrap-icons.css">
@@ -35,7 +35,7 @@
       <div class="spinner-grow text-primary" role="status"><span class="visually-hidden">Loading...</span></div>
     </div>
     <div id="app">
-    
+
 
         <main class="py-4">
             @yield('content')
