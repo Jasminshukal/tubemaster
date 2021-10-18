59afa7b103d5ad1166a9608c4459159e700e18ce
jasmin-shukla <mr.jasmin.shukal@gmail.com>
2021-10-18 14:52:07 +0530

    enw 01

--- resources/views/admin/equipment/index.blade.php
+++ resources/views/admin/equipment/index.blade.php
@@ -61,11 +61,11 @@
                         <table class="w-100" id="dataTable">
                             <thead>
                                 <tr>
-                                    <th width="100px">Name</th>
-                                    <th width="100px">Serial Number</th>
-                                    <th width="100px">Part</th>
-                                    <th width="100px">Status</th>
-                                    <th width="100px">Action</th>
+                                    <th width="200px">Name</th>
+                                    <th width="200px">Serial Number</th>
+                                    <th width="200px">Part</th>
+                                    <th width="200px">Status</th>
+                                    <th width="200px">Action</th>
                                 </tr>
                             </thead>
                             <tbody>
